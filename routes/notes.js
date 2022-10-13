51165dbbf68af9d09d67a1b030149df136ec9867
Brevenn <Brevenjh@gmail.com>
2022-10-13 14:13:27 -0500

    added note post function to add note to the page

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,53 @@
-const notes = require('express').Router();
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/');
-const { v4: uuidv4 } = require('uuid');
+const notes = require("express").Router();
+const { readFromFile, writeToFile, readAndAppend } = require("../helpers/");
+const { v4: uuidv4 } = require("uuid");
 
-notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-notes.get('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            const result = json.filter((note) => note.note_id === noteId);
-            return result.length > 0
-            ? res.json(result)
-            : res.json('No note with that ID found');
-        });
+notes.get("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID found");
+    });
 });
 
-notes.delete('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('./db/db.json')
-        .then((data) => JSON.parse(data))
-        .then((json) => {
-            const result = json.filter((note) => note.note_id !== noteId);
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id !== noteId);
 
-            writeToFile('./db/db.json', result);
+      writeToFile("./db/db.json", result);
 
-            res.json(`Item ${noteId} has been sent to the trash`);
-        });
-
+      res.json(`Item ${noteId} has been sent to the trash`);
+    });
 });
+
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully to the page`);
+  } else {
+    res.error("An error occurred adding your Note");
+  }
+});
+
+module.exports = notes;
